2e550ae0dd1091f3304afc66df744b019d3e2964
Fahim sahriar <fahimsahriar2002@gmail.com>
2026-08-23 16:54:04 +0600

    Add JWT token generation

--- app/auth_utils.py
+++ app/auth_utils.py
@@ -1,6 +1,12 @@
+import os
+from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
+from jose import jwt
+from dotenv import load_dotenv
 
+load_dotenv()
 
+# Password Hashing
 bcrypt_context = CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
@@ -19,3 +25,34 @@ def verify_password(
         plain_password,
         hashed_password
     )
+    
+# JWT Configuration
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ 
+ 
+# Create JWT
+
+def create_access_token(
+    username: str,
+    user_id: int
+):
+    encode = {
+        "sub": username,
+        "id": user_id
+    }
+    expires = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    encode.update({
+        "exp": expires
+    })
+    return jwt.encode(
+        encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    
