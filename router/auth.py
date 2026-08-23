d4f87b462052172a72653f140cfa4e3123f360c5
Fahim sahriar <fahimsahriar2002@gmail.com>
2026-08-23 17:48:26 +0600

    add login endpoint

--- router/auth.py
+++ router/auth.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from typing import Annotated
 
 from app.database import get_db
 from app.models import User
 from app.schemas import UserCreate, UserResponse
-from app.auth_utils import hash_password
+from app.auth_utils import (
+    create_access_token,
+    hash_password,
+    verify_password
+)
 
 
 router = APIRouter(
@@ -60,3 +65,38 @@ def register_user(
     db.refresh(user_model)
 
     return user_model
+
+# Login
+@router.post("/login")
+def login_user(
+    db: db_dependency,
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = db.query(User).filter(
+        User.username == form_data.username
+    ).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Username or password is incorrect"
+        )
+
+    if not verify_password(
+        form_data.password,
+        user.hashed_password
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Username or password is incorrect"
+        )
+
+    access_token = create_access_token(
+        username=user.username,
+        user_id=user.id
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
