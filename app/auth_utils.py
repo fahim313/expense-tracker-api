662034ae5cc4934cf36c349eded24f6d3f36bf02
Fahim sahriar <fahimsahriar2002@gmail.com>
2026-08-23 17:39:17 +0600

    Add authentication utilities and updated main.py

--- app/auth_utils.py
+++ app/auth_utils.py
@@ -1,8 +1,15 @@
 import os
 from datetime import datetime, timedelta, timezone
-from passlib.context import CryptContext
-from jose import jwt
+ 
 from dotenv import load_dotenv
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+ 
+from app.database import get_db
+from app.models import User
 
 load_dotenv()
 
@@ -55,4 +62,31 @@ def create_access_token(
         algorithm=ALGORITHM
     )
 
-    
+# Get current logged-in user
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+ 
+ 
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+ 
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("id")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+ 
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+ 
+    return user
--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from app.database import engine, Base
 from app import models
-
+from router import auth
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
